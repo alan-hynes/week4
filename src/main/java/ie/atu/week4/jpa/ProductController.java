5708c1f03fcf7f2d4ef9c1975641d1e89c773513
alan-hynes <G00400498@atu.ie>
2024-10-17 16:10:14 +0100

    created api endpoints for crud operations

--- src/main/java/ie/atu/week4/jpa/ProductController.java
+++ src/main/java/ie/atu/week4/jpa/ProductController.java
@@ -9,54 +9,34 @@ import java.util.List;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-    private List<Product> productList = new ArrayList<>();
-    public ProductController() {
-        productList.add(new Product("Tv", "A big tv", 500, 100));
-        productList.add(new Product("Radio", "A small radio", 100, 101));
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
+
     @GetMapping("/getProducts")
-    public List<Product> getProducts() {
-        return productList;
+    public ResponseEntity<List<Product>> getProducts() {
+        return ResponseEntity.ok(productService.getAll());
     }
 
     @PostMapping("/addProduct")
-    public ResponseEntity<List> addProduct(@RequestBody Product product) {
-        productList.add(product);
-        return ResponseEntity.ok(productList);
-    }
-
-    private Product findProductById(int id) {
-        for (Product product : productList) {
-            if (product.getProductCode() == id) {
-                return product;
-            }
-        }
-        return null;
+    public ResponseEntity<Product> addProduct(@RequestBody Product product) {
+        Product newProduct = productService.addProduct(product);
+        return ResponseEntity.ok(newProduct);
     }
 
     @PutMapping("/updateProduct/{id}")
-    public ResponseEntity<Product> updateProduct(@PathVariable int id, @RequestBody Product updatedProduct) {
-        Product existingProduct = findProductById(id);
-
-        if (existingProduct != null) {
-            existingProduct.setProductName(updatedProduct.getProductName());
-            existingProduct.setProductDescription(updatedProduct.getProductDescription());
-            existingProduct.setProductPrice(updatedProduct.getProductPrice());
-            return ResponseEntity.ok(existingProduct);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        Product updatedProduct = productService.updateProduct(id, product);
+        return ResponseEntity.ok(updatedProduct);
     }
 
     @DeleteMapping("/deleteProduct/{id}")
-    public ResponseEntity<List<Product>> deleteProduct(@PathVariable int id) {
-        Product existingProduct = findProductById(id);
-
-        if (existingProduct != null) {
-            productList.remove(existingProduct);
-            return ResponseEntity.ok(productList);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<Product> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.ok().build();
     }
+
 }
